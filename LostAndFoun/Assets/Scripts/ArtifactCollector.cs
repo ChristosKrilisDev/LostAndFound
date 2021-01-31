27734a5a95f3dc7de4047e700a4e27dc44f4d5d9
ChristosKrilisDev <christos.krilis.dev@gmail.com>
2021-01-31 13:18:20 +0200

    Init

---
+++ LostAndFoun/Assets/Scripts/ArtifactCollector.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArtifactCollector : MonoBehaviour
+{
+
+    public Door[] doorsToOpen;
+
+    public Item[] items;
+    int count = 0;
+
+    void Start()
+    {
+        foreach(Item item in items)
+        {
+            item.gameObject.SetActive(false);
+        }
+    }
+
+    public void AddItem(List<Item> newItem)
+    {
+        bool dBreak = false;
+
+        // Debug.Log("Find Artifact");
+        for(int i = 0; i < newItem.Count; i++)
+        {
+            for(int j = 0; j < items.Length; j++)
+            {
+                if(dBreak)
+                    break;
+
+                if(newItem[i].id == this.items[j].id)
+                {
+                    //sfx
+                    items[j].gameObject.SetActive(true);
+
+                    FindObjectOfType<Inventory>().RemoveItem(newItem[i]);
+                    count++;
+
+                    dBreak = true;
+
+                    break;
+                    
+                }                
+            }
+        }
+        if(count >= items.Length)
+        {
+            Debug.Log("Find Artifact");
+
+            doorsToOpen[0].DoorActivity();
+            doorsToOpen[1].DoorActivity();
+        }
+    }
+
+
+}
